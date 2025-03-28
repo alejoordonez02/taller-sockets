23a0ed2eaec933ebff100ad671aba0adc688fe4e
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-03-28 12:44:46 -0300

    import: copiar socket desde hands-on_sockets_in_cpp (GPL v2)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Sockets
+## Origen del código
+El componente `socket` fue tomado de [hands-on-sockets-in-cpp](https://github.com/eldipa/hands-on-sockets-in-cpp) y se encuentra bajo la licencia GPL v2.
+Este repositorio respeta la misma licencia para cumplir con los términos de distribución.
 
+## Licencia:
+GPL v2
---
+++ common_src/socket/liberror.h
@@ -0,0 +1,24 @@
+/*
+ * Este archivo fue tomado de hands-on-socket-in-cpp
+ * Repositorio original: https://github.com/eldipa/hands-on-sockets-in-cpp
+ * Licencia original: GPL v2
+ *
+ * De acuerdo con la GPL v2, este código se mantiene bajo la misma licencia.
+ */
+#ifndef LIB_ERROR_H
+#define LIB_ERROR_H
+
+#include <exception>
+
+class LibError : public std::exception {
+    char msg_error[256];
+
+    public:
+    LibError(int error_code, const char* fmt, ...) noexcept;
+
+    virtual const char* what() const noexcept;
+
+    virtual ~LibError();
+};
+
+#endif
